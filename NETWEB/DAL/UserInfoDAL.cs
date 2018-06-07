1f34ecd6b01d8c562794361ea040b301d48070a7
yuanyyb@hotmail.com <yuanyyb@hotmail.com>
2018-06-07 14:38:51 +0800

    基本架构搭建

---
+++ NETWEB/DAL/UserInfoDAL.cs
@@ -0,0 +1,13 @@
+﻿using IDAL;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DAL
+{
+    public class UserInfoDAL : BaseDAL<UserInfo>, IUserInfoDAL
+    {
+    }
+}
---
+++ NETWEB/DALFactory/AbstractFactory.cs
@@ -0,0 +1,34 @@
+﻿
+using IDAL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace DALFactory
+{
+    public class AbstractFactory
+    {
+        /// <summary>
+        /// 通过反射的形式创建数据操作类的实例
+        /// </summary>
+        /// <param name="fullClassName"></param>
+        /// <returns></returns>
+        public static object CreateInstance(string dalAssemblyPath, string fullClassName)
+        {
+            var assembly = Assembly.Load(dalAssemblyPath);
+            return assembly.CreateInstance(fullClassName);
+
+        }
+
+        public static IUserInfoDAL CreateUserInfoDAL() 
+        {
+            string classFulleName = ConfigurationManager.AppSettings["DalNamespace"] + ".UserInfoDAL";
+            var obj = CreateInstance(ConfigurationManager.AppSettings["DalAssembly"], classFulleName);
+            return obj as IUserInfoDAL;
+        }
+
+    }
+}
